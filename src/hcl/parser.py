dcc775802fcbe2b95997ac9b291130615488f04c
rererecursive <patternseverywhere@yandex.com>
2018-08-31 22:39:46 +1000

    Construct blocks during parsing.

--- src/hcl/parser.py
+++ src/hcl/parser.py
@@ -1,5 +1,6 @@
 
 from os.path import abspath, dirname, exists, join
+from enum import Enum
 import re, sys
 
 from lexer import Lexer
@@ -8,6 +9,7 @@ from ply import lex, yacc
 import inspect
 
 DEBUG = False
+ERROR_STATE = None
 
 # When using something like pyinstaller, the __file__ attribute isn't actually
 # set correctly, so the parse file isn't able to be saved anywhere sensible.
@@ -156,13 +158,24 @@ class HclParser(object):
                    | objectkey EQUAL object
                    | objectkey EQUAL list
         '''
+        line_no = p.lexer.lex.lineno - 2
+        block = self.working_block_stack[-1]
+        block.add_argument_item(key=p[1], value=p[3], line_no=line_no)
+
         if DEBUG:
             self.print_p(p)
         p[0] = (p[1], p[3])
 
     def p_objectitem_1(self, p):
         "objectitem : NL"
-        #print ('NL!', p.lexer.lex.lineno)
+        line_no = p.lexer.lex.lineno - 2
+
+        try:
+            block = self.working_block_stack[-1]
+            block.blank_lines.append(line_no)
+        except IndexError:
+            # We're not inside a block.
+            self.top.blank_lines.append(line_no)
 
     def p_objectitem_2(self, p):
         "objectitem : block"
@@ -173,6 +186,12 @@ class HclParser(object):
 
     def p_block_0(self, p):
         "block : blockId object"
+        # The end of a block.
+        line_no = p.lexer.lex.lineno - 2
+        block = self.working_block_stack.pop()
+        block.line_end = line_no
+        self.top.blocks.append(block)
+
         if DEBUG:
             self.print_p(p)
         p[0] = (p[1], p[2])
@@ -188,6 +207,22 @@ class HclParser(object):
         blockId : IDENTIFIER
                 | STRING
         '''
+        # The beginning of a block (i.e. a block definition).
+        line_no = p.lexer.lex.lineno - 1
+        try:
+            block = self.working_block_stack[-1]
+        except IndexError:
+            self.working_block_stack.append(Block(line_start=line_no))
+            block = self.working_block_stack[-1]
+
+        if block.line_start != line_no:
+            # A new block.
+            new_block = Block(line_start=line_no)
+            new_block.add_definition_item(p[1])
+            self.working_block_stack.append(new_block)
+        else:
+            block.add_definition_item(p[1])
+
         if DEBUG:
             self.print_p(p)
         p[0] = p[1]
@@ -317,6 +352,10 @@ class HclParser(object):
         self.yacc = yacc.yacc(module=self, debug=False, optimize=1, picklefile=pickle_file)
         self.lexer = Lexer()
 
+        self.top = Top()
+        self.working_block_stack = [] # For calculations
+
+
     def parse(self, s):
         results = {}
         lines = s.split('\n')
@@ -345,20 +384,43 @@ class Block():
     """
     def __init__(self, line_start=-1):
         self.line_start = line_start    # Token
-        self.line_end = line_end        # Token
+        self.line_end = -1              # Token
         self.blank_lines = []
-        self.definition = {}            # Definition
-        self.arguments = []             # Argument
+        self.definition = Definition()
+        self.arguments = Arguments()
+
+    def add_argument_item(self, key, value, line_no):
+        self.arguments.items.append({'key': key, 'value': value, 'line_no': line_no})
+
+    def add_definition_item(self, text):
+        self.definition.add_item(text)
 
 class Definition():
     """Represents the definition of a block, e.g. 'resource "aws_instance" "hello"'
     """
-    def __init__(self, keyword=None, type=None, none=None):
+    def __init__(self, keyword=None, type=None, name=None):
         self.keyword = keyword
         self.type = type
         self.name = name
 
-class Argument():
+    def add_item(self, text):
+        """For a definition of length 2, only use the keyword and name.
+        Otherwise, include the type.
+        """
+        if not self.keyword:
+            self.keyword = text
+        elif not self.name:
+            self.name = text
+        else:
+            if not self.type:
+                self.type = self.name
+                self.name = text
+            else:
+                if DEBUG:
+                    print ("ERROR: a block cannot have a definition of greater than 3 strings. Skipping...")
+                    # TODO: set error status and raise exception
+
+class Arguments():
     """Represents an argument in a block.
     """
     def __init__(self):
@@ -373,4 +435,6 @@ class Token():
         self.end = end
         self.text = text
 
+class Errors(Enum):
+    KEEP_PREVIOUS_STATE = 1
 
